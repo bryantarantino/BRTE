80893efe69f30022ae675c7f1487e27e29c53a94
ejohnson93 <ethanjjohnson93@hotmail.com>
2014-10-27 21:09:45 -0500

    Added Toggling to the Football Menu

--- BigRedTicketExchange/BigRedTicketExchange/Migrations/Configuration.cs
+++ BigRedTicketExchange/BigRedTicketExchange/Migrations/Configuration.cs
@@ -29,30 +29,30 @@ namespace BigRedTicketExchange.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
-            if (!context.Sports.Where(x => x.Name == "Football").Any())
-            {
-                context.Sports.AddOrUpdate(
-                new Sport { Name = "Football" }
-                );
-                context.SaveChanges();
-                context.Games.AddOrUpdate(
-                    new Game { Opponent="Rutgers", Date = new DateTime(2014, 10, 25), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single(), }
-                    );
-            }
-            context.Sports.AddOrUpdate(
-                new Sport { Name = "Volleyball" },
-                new Sport { Name = "Men's Basketball"}
-                );
-            context.SaveChanges();
-            context.Games.AddOrUpdate(
-                new Game { Opponent = "Florida Atlantic", Date = new DateTime(2014, 8, 30), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
-                new Game { Opponent = "McNeese State", Date = new DateTime(2014, 9, 6), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
-                new Game { Opponent = "Fresno State", Date = new DateTime(2014, 9, 13), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
-                new Game { Opponent = "Miami", Date = new DateTime(2014, 9, 20), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
-                new Game { Opponent = "Illinios", Date = new DateTime(2014, 9, 26), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
-                new Game { Opponent = "Purdue", Date = new DateTime(2014, 11, 1), IsActive = true, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
-                new Game { Opponent = "Minnesota", Date = new DateTime(2014, 11, 22), IsActive = true, Sport = context.Sports.Where(x => x.Name == "Football").Single() }
-                );
+            //if (!context.Sports.Where(x => x.Name == "Football").Any())
+            //{
+            //    context.Sports.AddOrUpdate(
+            //    new Sport { Name = "Football" }
+            //    );
+            //    context.SaveChanges();
+            //    context.Games.AddOrUpdate(
+            //        new Game { Opponent="Rutgers", Date = new DateTime(2014, 10, 25), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single(), }
+            //        );
+            //}
+            //context.Sports.AddOrUpdate(
+            //    new Sport { Name = "Volleyball" },
+            //    new Sport { Name = "Men's Basketball"}
+            //    );
+            //context.SaveChanges();
+            //context.Games.AddOrUpdate(
+            //    new Game { Opponent = "Florida Atlantic", Date = new DateTime(2014, 8, 30), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
+            //    new Game { Opponent = "McNeese State", Date = new DateTime(2014, 9, 6), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
+            //    new Game { Opponent = "Fresno State", Date = new DateTime(2014, 9, 13), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
+            //    new Game { Opponent = "Miami", Date = new DateTime(2014, 9, 20), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
+            //    new Game { Opponent = "Illinios", Date = new DateTime(2014, 9, 26), IsActive = false, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
+            //    new Game { Opponent = "Purdue", Date = new DateTime(2014, 11, 1), IsActive = true, Sport = context.Sports.Where(x => x.Name == "Football").Single() },
+            //    new Game { Opponent = "Minnesota", Date = new DateTime(2014, 11, 22), IsActive = true, Sport = context.Sports.Where(x => x.Name == "Football").Single() }
+            //    );
         }
     }
 }
